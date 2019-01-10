3e21aa7c7f2979b5d60f1ca6ca9500af3f3cbd49
Garret Blankenship <garret@leafcutter.com.au>
2019-01-10 12:27:08 +1100

    adding in jwt

---
+++ controllers/auth_controller.js
@@ -0,0 +1,17 @@
+const UserModel = require("./../database/models/user_model");
+
+function register(req, res, next) {
+    const { email, password } = req.body;
+
+    UserModel.register({ email }, password, function(err, user) {
+       if (err) { 
+           return next(err)
+        }
+      
+        
+    });
+}
+
+module.exports = {
+    register
+}
