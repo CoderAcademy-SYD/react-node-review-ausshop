c3b861bc78cb621e2dc68ad01ad64b4126306f49
Garret Blankenship <garret@leafcutter.com.au>
2019-01-11 11:53:57 +1100

    added product routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authRoutes = require("./auth_routes");
+const productRoutes = require("./product_routes");
 
 router.use("/auth", authRoutes);
+router.use("/products", productRoutes);
 
 module.exports = router;
